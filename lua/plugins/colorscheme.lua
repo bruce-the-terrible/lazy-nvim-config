4c9823cbf27ca585d6b94ac65b12465e6e636434
brucegong <brucegong@gmail.com>
2023-10-01 10:40:42 -0500

    add colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add molokai
+  { "tomasr/molokai" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "molokai",
+    },
+  }
+}
