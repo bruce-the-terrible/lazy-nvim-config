52e7a5fcca8f3f7cb30276c772b9d06407b59559
brucegong <brucegong@gmail.com>
2024-03-15 21:22:46 +0800

    Update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- close virtual text
-vim.diagnostic.config({ virtual_text = false })
+vim.opt.tabstop = 4
